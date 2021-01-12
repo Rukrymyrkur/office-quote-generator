ab4ab68f258bebc6911917d23305f48daf811950
Rukrymyrkur <surmilaviciute@gmail.com>
2021-01-12 13:34:30 +0200

    added readme file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import Quotes from './components/Quotes';
+import React from "react";
+import Quotes from "./components/Quotes";
 
 function App() {
   return (
     <div>
-    <div className="box">
-      <div className="quotes-title">The Office Quote Generator</div>
-      <Quotes />
-      <div className="description">
-        <p>Neringa Surmilavičiūtė 2020</p>
-        <a href="https://github.com/Rukrymyrkur/office-quote-generator" target="_blank">Source code</a>
+      <div className="box">
+        <div className="quotes-title">The Office Quote Generator</div>
+        <Quotes />
+        <div className="description">
+          <p>Neringa Surmilavičiūtė @ 2020</p>
+          <a
+            href="https://github.com/Rukrymyrkur/office-quote-generator"
+            target="_blank"
+          >
+            Source code
+          </a>
+        </div>
       </div>
-    </div>
     </div>
   );
 }
